b18e8632342ee670e183a96e4e78e532c0e73aa6
avpaul <vabimana@gmail.com>
2019-07-18 18:12:27 +0200

    initial commit

---
+++ src/serviceWorker.js
@@ -0,0 +1,38 @@
+// cache files and requests
+const cacheName = "files";
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll([
+        "/assets/logo_200.png",
+        "/index.js",
+        "/favicon-16x16.png",
+        "../favicon-32x32.png",
+        "https://fonts.googleapis.com/css?family=Comfortaa:300,400,500,600,700&display=swap"
+      ]);
+    })
+  );
+});
+
+// return requests with cached resources
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      const requestToCache = event.request.clone();
+      return fetch(requestToCache).then(response => {
+        if (!response || response.status !== 200) {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+        caches.open(cacheName).then(cache => {
+          cache.put(requestToCache, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
